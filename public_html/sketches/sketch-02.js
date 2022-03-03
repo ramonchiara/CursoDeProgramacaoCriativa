adf7d382b3acc09ac2dd389b09960ad7fb0fea15
Ramon Chiara <ramonchiara@gmail.com>
2022-03-02 22:02:04 -0300

    Sketch Transform - Final Drawing

--- public_html/sketches/sketch-02.js
+++ public_html/sketches/sketch-02.js
@@ -19,7 +19,7 @@ const sketch = () => {
         const w = width * 0.01;
         const h = height * 0.1;
 
-        const num = 12;
+        const num = 40;
         const radius = width * 0.3;
 
         let x, y;
@@ -34,13 +34,24 @@ const sketch = () => {
             context.save();
             context.translate(x, y);
             context.rotate(-angle);
-            context.scale(random.range(1, 3), 1);
+            context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
 
             context.beginPath();
             // context.rect(0, 0, w, h);
             // context.rect(x, y, w, h);
-            context.rect(-w * 0.5, -h * 0.5, w, h);
+            context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
             context.fill();
+            context.restore();
+
+            context.save();
+            context.translate(cx, cy);
+            context.rotate(-angle);
+
+            context.lineWidth = random.range(5, 20);
+
+            context.beginPath();
+            context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(1, -8), slice * random.range(1, 5));
+            context.stroke();
             context.restore();
     }
     };
