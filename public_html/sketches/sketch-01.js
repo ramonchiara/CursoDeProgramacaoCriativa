589bfde6ec004c0950809b881ee62b00f818e67d
Ramon Chiara <ramonchiara@gmail.com>
2022-03-02 21:11:46 -0300

    Fundamentals - usando o Canvas-Sketch.

---
+++ public_html/sketches/sketch-01.js
@@ -0,0 +1,51 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+    // dimensions: [600, 600]
+
+    /*
+     dimensions: 'A4',
+     pixelsPerInch: 300,
+     orientation: 'landscape'
+     */
+
+    dimensions: [1080, 1080] // instagram! :)
+};
+
+const sketch = () => {
+    return ({ context, width, height }) => {
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, width, height);
+        context.lineWidth = width * 0.01;
+
+        const w = width * 0.1; // 60;
+        const h = height * 0.1; // 60;
+        const gap = width * 0.03; // 20;
+        const ix = width * 0.17;
+        const iy = height * 0.17;
+
+        const off = width * 0.02;
+
+        let x, y;
+
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 5; j++) {
+                x = ix + (w + gap) * i;
+                y = iy + (h + gap) * j;
+
+                context.beginPath();
+                context.rect(x, y, w, h);
+                context.stroke();
+
+                if (Math.random() > 0.5) {
+                    context.beginPath();
+                    context.rect(x + off / 2, y + off / 2, w - off, h - off);
+                    context.stroke();
+                }
+            }
+    }
+
+    };
+};
+
+canvasSketch(sketch, settings);
