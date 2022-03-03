86fa3abc9f7475f815f486ec5aa8a2d360382347
Ramon Chiara <ramonchiara@gmail.com>
2022-03-02 23:15:13 -0300

    Agentes Sketch - final animation exported to mp4

--- public_html/sketches/sketch-03.js
+++ public_html/sketches/sketch-03.js
@@ -1,18 +1,11 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
     dimensions: [1080, 1080],
     animate: true
 };
-
-/*
-const animate = () => {
-    console.log('domestika');
-    requestAnimationFrame(animate);
-}
-animate();
-*/
 
 const sketch = ({ context, width, height }) => {
     const agents = [];
@@ -28,9 +21,30 @@ const sketch = ({ context, width, height }) => {
         context.fillStyle = 'white';
         context.fillRect(0, 0, width, height);
 
+        for (let i = 0; i < agents.length; i++) {
+            const agent = agents[i];
+
+            for (let j = i + 1; j < agents.length; j++) {
+                const other = agents[j];
+
+                const dist = agent.pos.getDistance(other.pos);
+
+                if (dist > 200)
+                    continue;
+
+                context.lineWidth = math.mapRange(dist, 0, 200, 12, 1);
+
+                context.beginPath();
+                context.moveTo(agent.pos.x, agent.pos.y);
+                context.lineTo(other.pos.x, other.pos.y);
+                context.stroke();
+            }
+        }
+
         agents.forEach(agent => {
             agent.update();
             agent.draw(context);
+            agent.bounce(width, height);
         })
     };
 };
@@ -43,6 +57,12 @@ class Vector {
         this.y = y;
         this.radius = radius;
     }
+
+    getDistance(v) {
+        const dx = this.x - v.x;
+        const dy = this.y - v.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
 }
 
 class Agent {
@@ -50,6 +70,13 @@ class Agent {
         this.pos = new Vector(x, y);
         this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
         this.radius = random.range(4, 12);
+    }
+
+    bounce(width, height) {
+        if (this.pos.x <= 0 || this.pos.x >= width)
+            this.vel.x *= -1;
+        if (this.pos.y <= 0 || this.pos.y >= height)
+            this.vel.y *= -1;
     }
 
     update() {
