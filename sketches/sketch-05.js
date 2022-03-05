5bc5c0b2da370536b839843ec9d5b686caf783b9
Ramon Chiara <ramonchiara@gmail.com>
2022-03-05 20:06:53 -0300

    Sketch Typing (?) - Adicionar Painel de Controle Exercise

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -1,8 +1,9 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const Tweakpane = require('tweakpane');
 
 const settings = {
-    dimensions: [1080, 1080]
+    dimensions: [1080, 1080],
 };
 
 let manager;
@@ -14,12 +15,21 @@ let fontFamily = 'serif';
 const typeCanvas = document.createElement('canvas');
 const typeContext = typeCanvas.getContext('2d');
 
+const params = {
+    glyph50: '',
+    glyph100: '.',
+    glyph150: '-',
+    glyph200: '+',
+    fgColor: 'rgb(255, 0, 0)',
+    bgColor: 'rgb(33, 66, 66)'
+};
+
 const sketch = ({ context, width, height }) => {
     const cell = 20;
     const cols = Math.floor(width / cell);
     const rows = Math.floor(height / cell);
     const numCells = cols * rows;
-    
+
     typeCanvas.width = cols;
     typeCanvas.height = rows;
 
@@ -53,13 +63,11 @@ const sketch = ({ context, width, height }) => {
 
         const typeData = typeContext.getImageData(0, 0, cols, rows).data;
 
-        context.fillStyle = 'black';
+        context.fillStyle = params.bgColor;
         context.fillRect(0, 0, width, height);
-        
+
         context.textBaseline = 'middle';
         context.textAlign = 'center';
-
-        // context.drawImage(typeCanvas, 0, 0);
 
         for (let i = 0; i < numCells; i++) {
             const col = i % cols;
@@ -75,24 +83,16 @@ const sketch = ({ context, width, height }) => {
 
             const glyph = getGlyph(r);
 
-            context.font = `${cell * 2}px ${fontFamily}`; 
+            context.font = `${cell * 2}px ${fontFamily}`;
             if (Math.random() < 0.1)
                 context.font = `${cell * 6}px ${fontFamily}`;
 
-            // context.fillStyle = `rgb(${r}, ${g}, ${b})`;
-            context.fillStyle = 'white';
+            context.fillStyle = params.fgColor;
 
             context.save();
             context.translate(x, y);
             context.translate(cell * 0.5, cell * 0.5);
-
-            // context.fillRect(0, 0, cell, cell);
             context.fillText(glyph, 0, 0);
-            /*
-             context.beginPath();
-             context.arc(0, 0, cell * 0.5, 0, Math.PI * 2);
-             context.fill();
-             */
             context.restore();
     }
     };
@@ -100,27 +100,39 @@ const sketch = ({ context, width, height }) => {
 
 const getGlyph = (v) => {
     if (v < 50)
-        return '';
+        return params.glyph50;
     if (v < 100)
-        return '.';
+        return params.glyph100;
     if (v < 150)
-        return '-';
+        return params.glyph150;
     if (v < 200)
-        return 'word';
+        return params.glyph200;
 
     const glyphs = '_= /'.split('');
 
     return random.pick(glyphs);
 };
 
-const onKeyUp = (e) => {
-    text = e.key.toUpperCase();
-    manager.render();
-};
+const createPane = () => {
+    const pane = new Tweakpane.Pane();
+    let folder;
 
-document.addEventListener('keyup', onKeyUp);
+    folder = pane.addFolder({title: 'Glyphs '});
+    folder.addInput(params, 'glyph50');
+    folder.addInput(params, 'glyph100');
+    folder.addInput(params, 'glyph150');
+    folder.addInput(params, 'glyph200');
+
+    folder = pane.addFolder({title: 'Colors '});
+    folder.addInput(params, 'fgColor');
+    folder.addInput(params, 'bgColor');
+    
+    pane.on('change', (ev) => { manager.render(); });
+};
 
 const start = async() => {
     manager = await canvasSketch(sketch, settings);
 };
+
+createPane();
 start();
