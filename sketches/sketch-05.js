bced64282a5edde5dc7f72aa8256cbf5b4fba6da
Ramon Chiara <ramonchiara@gmail.com>
2022-03-04 23:46:02 -0300

    Sketch Typing (?) - we have glyphs! \o/

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
     dimensions: [1080, 1080]
@@ -7,42 +8,109 @@ const settings = {
 let manager;
 
 let text = 'A';
-let fontSize = 800;
+let fontSize = 1200;
 let fontFamily = 'serif';
 
-const sketch = () => {
+const typeCanvas = document.createElement('canvas');
+const typeContext = typeCanvas.getContext('2d');
+
+const sketch = ({ context, width, height }) => {
+    const cell = 20;
+    const cols = Math.floor(width / cell);
+    const rows = Math.floor(height / cell);
+    const numCells = cols * rows;
+    
+    typeCanvas.width = cols;
+    typeCanvas.height = rows;
+
     return ({ context, width, height }) => {
-        context.fillStyle = 'white';
-        context.fillRect(0, 0, width, height);
+        typeContext.fillStyle = 'black';
+        typeContext.fillRect(0, 0, cols, rows);
 
-        context.fillStyle = 'black';
-        // context.font = fontSize + 'px ' + fontFamily;
-        context.font = `${fontSize}px ${fontFamily}`;
-        context.textBaseline = 'top';
-        // context.textBaseline = 'middle';
-        // context.textAlign = 'center';
+        fontSize = cols * 1.2;
+        typeContext.fillStyle = 'white';
+        typeContext.font = `${fontSize}px ${fontFamily}`;
+        typeContext.textBaseline = 'top';
 
-        const metrics = context.measureText(text);
+        const metrics = typeContext.measureText(text);
         const mx = metrics.actualBoundingBoxLeft * -1;
         const my = metrics.actualBoundingBoxAscent * -1;
         const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
         const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 
-        const x = (width - mw) * 0.5 - mx;
-        const y = (height - mh) * 0.5 - my;
+        const tx = (cols - mw) * 0.5 - mx;
+        const ty = (rows - mh) * 0.5 - my;
 
+        typeContext.save();
+        typeContext.translate(tx, ty);
 
-        context.save();
-        // context.translate(width * 0.5, height * 0.5);
-        context.translate(x, y);
+        typeContext.beginPath();
+        typeContext.rect(mx, my, mw, mh);
+        typeContext.stroke();
 
-        context.beginPath();
-        context.rect(mx, my, mw, mh);
-        context.stroke();
+        typeContext.fillText(text, 0, 0);
+        typeContext.restore();
 
-        context.fillText(text, 0, 0);
-        context.restore();
+        const typeData = typeContext.getImageData(0, 0, cols, rows).data;
+
+        context.fillStyle = 'black';
+        context.fillRect(0, 0, width, height);
+        
+        context.textBaseline = 'middle';
+        context.textAlign = 'center';
+
+        // context.drawImage(typeCanvas, 0, 0);
+
+        for (let i = 0; i < numCells; i++) {
+            const col = i % cols;
+            const row = Math.floor(i / cols);
+
+            const x = col * cell;
+            const y = row * cell;
+
+            const r = typeData[i * 4 + 0];
+            const g = typeData[i * 4 + 1];
+            const b = typeData[i * 4 + 2];
+            const a = typeData[i * 4 + 3];
+
+            const glyph = getGlyph(r);
+
+            context.font = `${cell * 2}px ${fontFamily}`; 
+            if (Math.random() < 0.1)
+                context.font = `${cell * 6}px ${fontFamily}`;
+
+            // context.fillStyle = `rgb(${r}, ${g}, ${b})`;
+            context.fillStyle = 'white';
+
+            context.save();
+            context.translate(x, y);
+            context.translate(cell * 0.5, cell * 0.5);
+
+            // context.fillRect(0, 0, cell, cell);
+            context.fillText(glyph, 0, 0);
+            /*
+             context.beginPath();
+             context.arc(0, 0, cell * 0.5, 0, Math.PI * 2);
+             context.fill();
+             */
+            context.restore();
+    }
     };
+};
+
+const getGlyph = (v) => {
+    if (v < 50)
+        return '';
+    if (v < 100)
+        return '.';
+    if (v < 150)
+        return '-';
+    if (v < 200)
+        return 'word';
+
+    const glyphs = '_= /'.split('');
+
+    return random.pick(glyphs);
 };
 
 const onKeyUp = (e) => {
@@ -56,32 +124,3 @@ const start = async() => {
     manager = await canvasSketch(sketch, settings);
 };
 start();
-
-/*
-const url = 'https://picsum.photos/200';
-
-const loadMeSomeImage = (url) => {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve(img);
-        img.onerror = () => reject();
-        img.src = url;
-    });
-};
-
-const start = () => {
-    loadMeSomeImage(url).then(img => {
-        console.log('image width', img.width);
-    });
-    console.log('this line');
-};
-
-const startSync = async () => {
-    const img = await loadMeSomeImage(url);
-    console.log('image width', img.width);
-    console.log('this line');
-};
-
-// start();
-startSync();
-*/
